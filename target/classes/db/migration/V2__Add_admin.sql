92d40bb25da0a283e5a8259409b419379be35a56
Artem Predvoditelev <vladglad39@gmail.com>
2018-05-28 21:11:14 +0300

    delete target

--- target/classes/db/migration/V2__Add_admin.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (id, activation_code, address, email, first_name, active, last_name, password, phone, username)
-      VALUES (1, null, 'localhost', 'grouper.merayalab@gmail.com',
-              'Admin', TRUE,'Administrator', '2pRXCePJLjyH6xG4', '+380999-99-99-999', 'Administrator');
-
-INSERT INTO user_role (user_id, roles)
-      VALUES (1, 'SUPERUSER');
