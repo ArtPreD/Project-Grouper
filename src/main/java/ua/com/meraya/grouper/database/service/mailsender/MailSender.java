8635b13b1a36cebca1e116f6bdcf2817b1dcf383
Artem Predvoditelev <vladglad39@gmail.com>
2018-05-30 13:47:32 +0300

    bugfix

--- src/main/java/ua/com/meraya/grouper/database/service/mailsender/MailSender.java
+++ src/main/java/ua/com/meraya/grouper/database/service/mailsender/MailSender.java
@@ -1,6 +1,5 @@
 package ua.com.meraya.grouper.database.service.mailsender;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -8,11 +7,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MailSender {
-    @Autowired
-    private JavaMailSender mailSender;
+
+    private final JavaMailSender mailSender;
 
     @Value("${spring.mail.username}")
     private String username;
+
+    public MailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     public void send(String mailTo, String subject, String message){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
